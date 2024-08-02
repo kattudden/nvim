c194ae137d8e6e78a92ce73c8e6e2e4deb9bab41
Michael Lehmann <lehmschi@gmail.com>
2024-08-02 14:23:34 +0200

    Remove dublicte setting

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -6,7 +6,3 @@ vim.opt.linebreak = true -- Line break on whole words
 -- Allow j/k when navigating wrapped lines
 vim.keymap.set("n", "j", "gj")
 vim.keymap.set("n", "k", "gk")
-
--- Spell check
-vim.opt.spelllang = 'de_ch'
-vim.opt.spell = true
