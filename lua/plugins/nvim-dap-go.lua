70907bc51159623000e421789e393b9092c596eb
Michael Lehmann <lehmschi@gmail.com>
2024-08-03 10:00:21 +0200

    Remmove

--- lua/plugins/nvim-dap-go.lua
+++
@@ -1,9 +0,0 @@
-return {
-    -- https://github.com/mfussenegger/nvim-dap
-    'mfussenegger/nvim-dap',
-    ft = 'go',
-    dependencies = {},
-    config = function ()
-    require('dap').setup()
-  end
-}
