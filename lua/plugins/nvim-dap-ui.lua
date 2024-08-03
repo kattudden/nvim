5193a03d4c1b4e23a2e4d2299ac718d5dd901b34
Michael Lehmann <lehmschi@gmail.com>
2024-08-03 09:58:36 +0200

    Add go adapter

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -115,13 +115,23 @@ return {
     dap.configurations = {
         go = {
             {
-            type = "go", -- Which adapter to use
-            name = "Debug", -- Human readable name
-            request = "launch", -- Whether to "launch" or "attach" to program
-            program = "${file}", -- The buffer you are focused on when running nvim-dap
+                type = "go", -- Which adapter to use
+                name = "Debug", -- Human readable name
+                request = "launch", -- Whether to "launch" or "attach" to program
+                program = "${file}", -- The buffer you are focused on when running nvim-dap
             },
         }
     }
+
+    dap.adapters.go = {
+        type = "server",
+        port = "${port}",
+        executable = {
+            command = vim.fn.stdpath("data") .. '/mason/bin/dlv',
+            args = { "dap", "-l", "127.0.0.1:${port}" },
+        },
+    }
+
   end
 }
 
